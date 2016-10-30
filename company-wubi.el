7d6d000e51d3ad18ea7b6332e699572d2c38eedb
Yevgnen Koh <wherejoystarts@gmail.com>
2016-10-30 10:16:12 +0800

    Define a local wubi-mode.
    Update README.org.

--- company-wubi.el
+++ company-wubi.el
@@ -33,7 +33,7 @@
 (require 's)
 
 (defgroup company-wubi nil
-  "Completion back-end for WUBI."
+  "Completion back-end for wubi input."
   :group 'company)
 
 (defcustom company-wubi-wb-dict-file
@@ -65,95 +65,96 @@ A string containing the name or the full path of the dict."
 
 (defcustom company-wubi-idle-delay
   0
-  "Overwrite the `company-idle-delay' variable."
+  "Hijack the `company-idle-delay' variable."
   :group 'company-wubi)
 
 (defcustom company-wubi-backends
   '(company-wubi)
-  "Overwrite the `company-backends' variable."
+  "Hijack the `company-backends' variable."
   :group 'company-wubi
   :type 'list)
 
 (defcustom company-wubi-minimum-prefix-length
   1
-  "Overwrite the `company-minimum-prefix-length' variable."
+  "Hijack the `company-minimum-prefix-length' variable."
   :group 'company-wubi
   :type 'number)
 
 (defcustom company-wubi-tooltip-limit
   5
-  "Overwrite the `company-tooltip-limit' variable."
+  "Hijack the `company-tooltip-limit' variable."
   :group 'company-wubi
   :type 'number)
 
 (defcustom company-wubi-tooltip-align-annotations
   nil
-  "Overwrite the `company-tooltip-align-annotations' variable."
+  "Hijack the `company-tooltip-align-annotations' variable."
   :group 'company-wubi
   :type 'boolean)
 
 (defcustom company-wubi-auto-complete-chars
-  '((input " " output  nil command nil)
-    (input "-" output  nil command nil)
-    (input "=" output  nil command nil)
-    (input ";" output  nil command nil)
-    (input "'" output  nil command nil)
-    (input "," output "，" command nil)
-    (input "." output "。" command nil)
-    (input "~" output "～" command nil)
-    (input "!" output "！" command nil)
-    (input "@" output "‧" command nil)
-    (input "#" output "＃" command nil)
-    (input "$" output "￥" command nil)
-    (input "%" output "％" command nil)
-    (input "^" output "……" command nil)
-    (input "&" output "＆" command nil)
-    (input "*" output "×" command nil)
-    (input "(" output "（" command nil)
-    (input ")" output "）" command nil)
-    (input "_" output "—" command nil)
-    (input "+" output "＋" command nil)
-    (input "{" output "｛" command nil)
-    (input "}" output "｝" command nil)
-    (input "[" output "［" command nil)
-    (input "]" output "］" command nil)
-    (input "\\" output "、" command nil)
-    (input ":" output "：" command nil)
-    (input "\"" output "“”" command nil)
-    (input "|" output "｜" command nil)
-    (input "?" output "？" command nil)
-    (input "/" output "、" command nil)
-    (input "<" output "《》" command nil)
-    (input ">" output "〈〉" command nil))
+  '((" ")
+    ("-")
+    ("=")
+    (";")
+    ("'")
+    ("," "，")
+    ("." "。")
+    ("~" "～")
+    ("!" "！")
+    ("@" "‧")
+    ("#" "＃")
+    ("$" "￥")
+    ("%" "％")
+    ("^" "……")
+    ("&" "＆")
+    ("*" "×")
+    ("(" "（")
+    (")" "）")
+    ("_" "—")
+    ("+" "＋")
+    ("{" "｛")
+    ("}" "｝")
+    ("[" "［")
+    ("]" "］")
+    ("\\" "、")
+    (":" "：")
+    ("\"" "“”")
+    ("|" "｜")
+    ("?" "？")
+    ("/" "、")
+    ("<" "《》")
+    (">" "〈〉"))
   "The auto complete char mapping in list form.
 
-Use `input' to trigger insert `output' when completing.
-The `command' field should be set to `nil'."
+The `car' of each element is used to trigger auto completion
+or controlling keys like page flipping or candidate selection.
+
+Currently -/= is used for page flipping and SPC is used for auto completion,
+while ;/' is used for 2nd and 3rd candidate selection.
+
+When the `cdr' of the element is not `nil', the `car' of the
+element will be inserted."
   :group 'company-wubi)
 
 (defcustom company-wubi-show-codes-in-pinyin
   t
   "Show wubi codes when using pinyin.
 
-Currently this will slow down the completion progress."
+Currently this will slow down the completion progress since
+the wubi code of all the candidates will be lookup in realtime."
   :group 'company-wubi
   :type 'boolean)
 
-;; Internal variables
+;;; Internal variables
 (defvar company-wubi-wb-table nil)
 (defvar company-wubi-py-table nil)
 (defvar company-wubi-wb-reverse-table nil)
-(defvar company-wubi-enable-p nil)
 (defvar company-wubi-mark-pair 0)
 (defvar company-wubi-p t)
+(defvar company-wubi-active-map (make-sparse-keymap))
 
-(define-minor-mode wubi-indication-mode
-  "Toggle Wubi indication mode on or off.
-Turn Wubi indication mode on if ARG is positive, off otherwise."
-  :group 'company-wubi
-  :global t
-  :lighter " 五")
-
+;;; Dictionary management
 (defun company-wubi--load-default-dict (file var)
   (with-temp-buffer
     (insert-file-contents file)
@@ -163,23 +164,41 @@ Turn Wubi indication mode on if ARG is positive, off otherwise."
                (-map #'s-trim
                      (s-split "\n" (buffer-string)))))))
 
-(defun company-wubi--load-dict ()
-  "Load the dictionaries."
+(defun company-wubi--load-dicts ()
+  "Load all the dictionaries.
+
+Including the `wb_table', `py_table' and `wb_reverse_table'."
   (company-wubi--load-default-dict company-wubi-wb-dict-file 'company-wubi-wb-table)
   (company-wubi--load-default-dict company-wubi-py-dict-file 'company-wubi-py-table)
   (company-wubi--load-default-dict company-wubi-wb-reverse-dict-file 'company-wubi-wb-reverse-table))
 
+(defun company-wubi--load-dicts-when-necessary ()
+  (unless
+      (and company-wubi-wb-table
+           company-wubi-py-table
+           company-wubi-wb-reverse-table)
+    (company-wubi--load-dicts)))
+
 (defun company-wubi--unload-dict ()
-  "Unload the dictionaries"
+  "Unload the dictionaries."
   (setq company-wubi-wb-table nil)
   (setq company-wubi-py-table nil)
   (setq company-wubi-wb-reverse-table nil))
 
+;;; Candidates
 (defun company-wubi--prefix ()
   "Get a prefix from current position."
-  (when (looking-back "`?[a-z]+" 0 t)
-    (let ((str (match-string 0)))
-      str)))
+  (let ((case-fold-search nil))
+    (when (looking-back "`?[a-z]+" 0 t)
+      (let ((prefix (substring-no-properties (match-string 0))))
+        (if (s-starts-with? "`" prefix)
+            (progn
+              (setq company-wubi-p nil)
+              prefix)
+          (progn
+            (setq company-wubi-p t)
+            (if (< (length prefix) 5)
+                prefix)))))))
 
 (defun company-wubi--py-candidates (prefix)
   "Return the candidates under pinyin input given `prefix'."
@@ -210,13 +229,9 @@ Turn Wubi indication mode on if ARG is positive, off otherwise."
 
 (defun company-wubi--candidates (prefix)
   "Return the candidates under given `prefix'."
-  (if (s-starts-with? "`" prefix)
-      (progn
-        (setq company-wubi-p nil)
-        (company-wubi--py-candidates (substring-no-properties prefix 1)))
-    (progn
-      (setq company-wubi-p t)
-      (company-wubi--wb-candidates prefix))))
+  (if company-wubi-p
+      (company-wubi--wb-candidates prefix)
+    (company-wubi--py-candidates (substring-no-properties prefix 1))))
 
 (defun company-wubi--wb-reverse-lookup-one (candidate)
   (let ((index (--find-index (equal candidate (car it)) company-wubi-wb-reverse-table)))
@@ -225,7 +240,14 @@ Turn Wubi indication mode on if ARG is positive, off otherwise."
       nil)))
 
 (defun company-wubi--wb-reverse-lookup (candidate)
-  "Return the wubi code of the `candidate'."
+  "Return the wubi code of the `candidate'.
+
+The wubi code is generated in the following rules.
+(A-Z denotes chinese characters, a-z denote wubi codes in order.)
+
+When the Chinese characters is AB, use code aabb,
+when ABC use abcc, when ABCD use abcd, when ABC...Z, use abcz.
+"
   (when company-wubi-show-codes-in-pinyin
     (or (company-wubi--wb-reverse-lookup-one candidate)
         (let ((len (length candidate)))
@@ -307,106 +329,105 @@ Turn Wubi indication mode on if ARG is positive, off otherwise."
    (- company-selection
       (1- company-wubi-tooltip-limit))))
 
-(defun company-wubi--bind-auto-complete-keys ()
-  "Bind the auto complete keys like `,', `.'."
-  (setq company-wubi-auto-complete-chars
-        (-map (lambda (mapping)
-                (let ((in (plist-get mapping 'input))
-                      (out (plist-get mapping 'output)))
-                  ;; Save the original bindings
-                  (setq mapping (plist-put mapping
-                                           'command (lookup-key company-active-map in)))
-                  ;; Define new bindings
-                  (define-key company-active-map in
-                    `(lambda ()
-                       (interactive)
-                       ;; Select the current candidate or 2nd/3rd candidate or change page
-                       (cond ((string= ,in ";") (company-complete-number 2))
-                             ((string= ,in "'") (company-complete-number 3))
-                             ((string= ,in "-") (company-wubi-previous-page))
-                             ((string= ,in "=") (company-wubi-next-page))
-                             (t (company-complete-selection)))
-                       ;; Input the auto complete char
-                       (let ((len (length ,out)))
-                         (cond ((= len 0)
-                                nil)
-                               ((= len 1)
-                                (insert ,out))
-                               ((= len 2)
-                                (progn
-                                  (insert (elt ,out company-wubi-mark-pair))
-                                  (setq company-wubi-mark-pair (- 1 company-wubi-mark-pair))))))))
-                  mapping))
-              company-wubi-auto-complete-chars)))
+(defun company-wubi--bind-keys ()
+  "Bind completion keys and other keys."
+  (let ((oldmap company-active-map)
+        (newmap (make-sparse-keymap)))
+    (set-keymap-parent newmap oldmap)
 
-(defun company-wubi--unbind-auto-complate-keys ()
-  "Unbind all auto complete keys."
-  (-map (lambda (mapping)
-          (let ((in (plist-get mapping 'input)))
-            (define-key company-active-map in
-              (plist-get company-wubi-auto-complete-chars 'command))
-            (setq mapping (plist-put mapping 'command nil))))
-        company-wubi-auto-complete-chars))
+    ;; TODO: unbind the number keys
+    (dotimes (i 10)
+      (define-key newmap (read-kbd-macro (format "%d" i)) 'company-complete-number))
+
+    (-map (lambda (mapping)
+            (let ((in (car mapping))
+                  (out (cdr mapping)))
+              ;; Define new bindings
+              (define-key newmap in
+                `(lambda ()
+                   (interactive)
+                   ;; Select the current candidate or 2nd/3rd candidate or change page
+                   (cond ((string= ,in ";") (company-complete-number 2))
+                         ((string= ,in "'") (company-complete-number 3))
+                         ((string= ,in "-") (company-wubi-previous-page))
+                         ((string= ,in "=") (company-wubi-next-page))
+                         (t (company-complete-selection)))
+                   ;; Insert the auto complete char
+                   (let ((len (length ,out)))
+                     (cond ((= len 0)
+                            nil)
+                           ((= len 1)
+                            (insert ,out))
+                           ((= len 2)
+                            (progn
+                              (insert (elt ,out company-wubi-mark-pair))
+                              (setq company-wubi-mark-pair (- 1 company-wubi-mark-pair))))))))
+              mapping))
+          company-wubi-auto-complete-chars)
+    newmap))
+
+;;; Advices for hihacking company mode
+(defun company-wubi--call-frontends (orig-fun &rest args)
+  (if wubi-mode
+      (let ((company-minimum-prefix-length company-wubi-minimum-prefix-length)
+            (company-tooltip-limit company-wubi-tooltip-limit)
+            (company-tooltip-align-annotations company-wubi-tooltip-align-annotations))
+        (apply orig-fun args))
+    (apply orig-fun args)))
+
+(defun company-wubi--idle (orig-fun &rest args)
+  (if wubi-mode
+      (let ((company-idle-delay company-wubi-idle-delay))
+        (apply orig-fun args))
+    (apply orig-fun args)))
+
+(defun company-wubi--good-prefix-p (orig-fun &rest args)
+  (if wubi-mode
+      (let ((company-minimum-prefix-length company-wubi-minimum-prefix-length))
+        (apply orig-fun args))
+    (apply orig-fun args)))
+
+(defun company-wubi--perform (orig-fun &rest args)
+  (if wubi-mode
+      (let ((company-active-map company-wubi-active-map))
+        (apply orig-fun args))
+    (apply orig-fun args)))
+
+;;; Wubi mode
+(define-minor-mode wubi-mode
+  "Toggle Wubi indication mode on or off.
+Turn Wubi indication mode on if ARG is positive, off otherwise."
+  :group 'company-wubi
+  :lighter " 五"
+  (if wubi-mode
+      (company-wubi-enable)
+    (company-wubi-disable)))
 
 (defun company-wubi-enable ()
   "Enable the wubi input method."
-  (interactive)
   ;; Load the input dict
-  (unless company-wubi-enable-p
-    (company-wubi--load-dict)
+  (company-wubi--load-dicts-when-necessary)
 
-    ;; Set company for better input experience
-    (company-wubi--localize-variable 'company-idle-delay company-wubi-idle-delay)
-    (company-wubi--localize-variable 'company-backends company-wubi-backends)
-    (company-wubi--localize-variable 'company-minimum-prefix-length company-wubi-minimum-prefix-length)
-    (company-wubi--localize-variable 'company-tooltip-limit company-wubi-tooltip-limit)
-    (company-wubi--localize-variable 'company-tooltip-align-annotations company-wubi-tooltip-align-annotations)
+  ;; Hijack the company settings
+  (company-wubi--localize-variable 'company-backends company-wubi-backends)
+  (advice-add 'company-call-frontends :around #'company-wubi--call-frontends)
+  (advice-add 'company--idle-delay :around #'company-wubi--idle)
+  (advice-add 'company--good-prefix-p :around #'company-wubi--good-prefix-p)
+  (advice-add 'company--perform :around #'company-wubi--perform)
 
-    ;; Set keys
-    ;; TODO: Unbind the number keys
-    (dotimes (i 10)
-      (define-key company-active-map (read-kbd-macro (format "%d" i)) 'company-complete-number))
-    (company-wubi--bind-auto-complete-keys)
-
-    (setq company-wubi-enable-p t)
-    (message "Wubi input enabled!")))
+  ;; Bind keys
+  (setq company-wubi-active-map (company-wubi--bind-keys)))
 
 (defun company-wubi-disable ()
   "Disable the wubi input method."
-  (interactive)
-  (when company-wubi-enable-p
-    ;; Unload the input dict
-    (company-wubi--unload-dict)
+  ;; Restore settings
+  (company-wubi--delocalize-variable 'company-backends))
 
-    ;; Restore the company configs
-    (company-wubi--delocalize-variable 'company-idle-delay)
-    (company-wubi--delocalize-variable 'company-backends)
-    (company-wubi--delocalize-variable 'company-minimum-prefix-length)
-    (company-wubi--delocalize-variable 'company-tooltip-limit)
-    (company-wubi--delocalize-variable 'company-tooltip-align-annotations)
-
-    ;; Unbind keys
-    (company-wubi--unbind-auto-complate-keys)
-
-    (setq company-wubi-enable-p nil)
-    (message "Wubi input disabled!")))
-
-(defun company-wubi-toggle ()
-  "Toggle the wubi input method."
-  (interactive)
-  (if company-wubi-enable-p
-      (progn
-        (company-wubi-disable)
-        (wubi-indication-mode -1))
-    (progn
-      (company-wubi-enable)
-      (wubi-indication-mode 1))))
-
-(global-set-key (kbd "C-\\") #'company-wubi-toggle)
+(global-set-key (kbd "C-\\") #'wubi-mode)
 
 ;;;###autoload
 (defun company-wubi (command &optional arg &rest ignored)
-  "`company-mode' completion back-end for `wubi-code.el'.
+  "`company-mode' completion back-end for wubi.
 Provide completion info according to COMMAND and ARG.  IGNORED, not used."
   (interactive (list 'interactive))
   (cl-case command
